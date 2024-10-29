cbdce9485d9b624e34f8d4127c83fe6e1e469ff3
Simone Guscetti <simone.guscetti@gmail.com>
2024-10-29 11:19:10 +0100

    Add day2 to main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 mod day1;
+mod day2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +11,7 @@ fn main() {
     match day.as_str() {
         "1a" => day1::day1a(file_path),
         "1b" => day1::day1b(file_path),
+        "2a" => day2::day2a(file_path),
         _ => println!("No valid day!"),
     }
 }
